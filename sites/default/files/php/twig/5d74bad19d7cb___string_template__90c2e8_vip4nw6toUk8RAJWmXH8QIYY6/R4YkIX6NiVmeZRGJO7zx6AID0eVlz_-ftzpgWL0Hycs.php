7423c07598cb18cc3d2906c51f5f9e2b2982ea91
Chaitanya <mr.chaitanya86@gmail.com>
2019-09-09 09:58:02 +0000

    RESTful API development

---
+++ sites/default/files/php/twig/5d74bad19d7cb___string_template__90c2e8_vip4nw6toUk8RAJWmXH8QIYY6/R4YkIX6NiVmeZRGJO7zx6AID0eVlz_-ftzpgWL0Hycs.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__90c2e8cad2bd8f22fec7c18d3a28fbfb89773b1620f19f561c10bcaf40cc2e2e */
+class __TwigTemplate_4a9130d1a24669a7154ecdd99a71b8cfecdef32908c97eddcc3ba46fdf9e2b9a extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<code>/event/{nid}</code>: PATCH, DELETE, GET";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__90c2e8cad2bd8f22fec7c18d3a28fbfb89773b1620f19f561c10bcaf40cc2e2e";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__90c2e8cad2bd8f22fec7c18d3a28fbfb89773b1620f19f561c10bcaf40cc2e2e", "");
+    }
+}
